f81bb486cce12423fb9cbabb842bee6a6f9bc9a9
stryker john de guzman <strykerjerk@gmail.com>
2026-03-23 13:41:33 +0800

    logi-texte

--- login.php
+++ login.php
@@ -119,7 +119,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>E-Commerce HR3 · Admin Portal</title>
+    <title>E-Commerce HR3 · Admin-Portal</title>
     
     <!-- Bootstrap 5 -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
